7a44a620a36e2634e3955092aee1405481a5690e
Zuxi <gpg@mail.imzuxi.com>
2024-06-08 13:21:37 -0400

    added a literal } thats it

--- Modules/FriendRequest/Json/WSNotification.cs
+++ Modules/FriendRequest/Json/WSNotification.cs
@@ -20,33 +20,34 @@ namespace Zuxi.OSC.Modules.FriendRequest.Json
     public class WebsocketContainer
     {
         public class Content
-    {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("type")]
-        public string ContentType { get; set; }
-
-        [JsonProperty("senderUserId")]
-        public string SenderUserId { get; set; }
-
-        [JsonProperty("senderUsername")]
-        public string SenderUsername { get; set; }
-
-        [JsonProperty("receiverUserId")]
-        public string ReceiverUserId { get; set; }
-
-        [JsonProperty("message")]
-        public string Message { get; set; }
-
-        [JsonProperty("details")]
-        public object Details { get; set; }
-
-        [JsonProperty("created_at")]
-        public DateTime CreatedAt { get; set; }
-        public static Content FromJson(string json)
         {
-            return JsonConvert.DeserializeObject<Content>(json);
+            [JsonProperty("id")]
+            public string Id { get; set; }
+
+            [JsonProperty("type")]
+            public string ContentType { get; set; }
+
+            [JsonProperty("senderUserId")]
+            public string SenderUserId { get; set; }
+
+            [JsonProperty("senderUsername")]
+            public string SenderUsername { get; set; }
+
+            [JsonProperty("receiverUserId")]
+            public string ReceiverUserId { get; set; }
+
+            [JsonProperty("message")]
+            public string Message { get; set; }
+
+            [JsonProperty("details")]
+            public object Details { get; set; }
+
+            [JsonProperty("created_at")]
+            public DateTime CreatedAt { get; set; }
+            public static Content FromJson(string json)
+            {
+                return JsonConvert.DeserializeObject<Content>(json);
+            }
         }
     }
 }
