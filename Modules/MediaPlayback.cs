c496da256d345a2e4187fdea5c77002dc55a2bdb
Zuxi <gpg@mail.imzuxi.com>
2024-06-15 22:34:25 -0400

    This Fixes a issue with Media playback crashing when nothing is actively playing

--- Modules/MediaPlayback.cs
+++ Modules/MediaPlayback.cs
@@ -1,13 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Nodes;
-using System.Threading.Tasks;
-using Windows.Media.Control;
+﻿using Windows.Media.Control;
 namespace Zuxi.OSC.Modules
 {
     internal class MediaPlayback
@@ -20,26 +11,31 @@ namespace Zuxi.OSC.Modules
             {
                 // GOD THIS IS SO UNOPTIMIZXED HOLY but cannot be asked to fix
                 // btw this took 3 hours to figure out since this project needed to be upgraded to .net core so i used net 8 org now 6 lmao 
+                try
+                {
+                    var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+                    var currentsession = gsmtcsm.GetCurrentSession();
+                    if (currentsession == null)
+                    {
+                        return "";
+                    }
+                    var playbackInfo = currentsession.GetPlaybackInfo();
 
-                var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+                    Console.WriteLine(playbackInfo.PlaybackStatus.ToString());
+                    if (playbackInfo.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Paused)
+                    {
+                        return "";
+                    }
 
-                var playbackInfo = gsmtcsm.GetCurrentSession().GetPlaybackInfo();
-
-                Console.WriteLine(playbackInfo.PlaybackStatus.ToString());
-                if (playbackInfo.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Paused)
+                    var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
+                    // Apple Music is kinda stupid ngl this is to fix it i know its stupid idc
+                    string artist = string.IsNullOrEmpty(mediaProperties.Artist) ? GetStringBeforeFirstDash(mediaProperties.AlbumArtist) : mediaProperties.Artist;
+                    return string.Format("{0} - {1}", mediaProperties.Title, artist);
+                }
+                catch (Exception e)
                 {
                     return "";
                 }
-
-                var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
-
-
-
-                //  if (mediaProperties.)
-                // Apple Music is kinda stupid ngl this is to fix it i know its stupid idc
-                string artist = string.IsNullOrEmpty(mediaProperties.Artist) ? GetStringBeforeFirstDash(mediaProperties.AlbumArtist) : mediaProperties.Artist;
-                 return string.Format("{0} - {1}", mediaProperties.Title, artist);
-
             }).Result;
         }
 
